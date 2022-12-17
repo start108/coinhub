a34cc5c3c8180ab1b8b2156c941bb7c89a0c6ea3
cjy <start108@naver.com>
2022-12-17 14:35:05 +0900

    Git 정상작동 테스트

--- src/main/java/com/jy/coinhub/CoinhubApplication.java
+++ src/main/java/com/jy/coinhub/CoinhubApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CoinhubApplication {
 
+	// Git 정상작동 테스트
 	public static void main(String[] args) {
 		SpringApplication.run(CoinhubApplication.class, args);
 	}
